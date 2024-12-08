81be5220b8b22bc3c7af7fdee0e2f8944a9fa2d6
hw2782 <hw2782@nyu.edu>
2024-12-07 22:21:18 -0500

    Fix GitHub Actions workflow to include correct test file path

--- db-service/test_db_service.py
+++ db-service/test_db_service.py
@@ -14,8 +14,14 @@ from db_service import (
     edit_transcription_collection,
 )
 from dotenv import load_dotenv
+from pymongo import MongoClient
+load_dotenv()
 
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME")
 
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
 @pytest.fixture(scope="function")
 def test_client():
     load_dotenv()
